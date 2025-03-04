0cc22cca5ac873c732a58723a99f2b197583ab2e
shiauno <cindy36908926@gmail.com>
2025-03-04 05:41:01 +0000

    Deploying to gh-pages from @ shiauno/vuetify-project@2b7fc6f052720d17b6d5a292b93633b81985b360 🚀

---
+++ assets/ssrBoot-CHpEwc53.js
@@ -0,0 +1,1 @@
+import{s,o as t,c as e,bK as n}from"./index-BIJ0zr-r.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
